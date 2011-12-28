c9535c82adec5118e0e9cf7ad187aa152dc1617c
Alexander Kozovski <Alexander@Alexander-Kozovskis-MacBook-Pro.local>
2011-12-28 00:24:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RainSensor::Application.config.secret_token = '0caf6a4a4bd1fce762b4b24aee2e2d06f49689d4633ba58241c29dd828c28c3f04b020617db8bb28e0a1cc2d3dfe5c98fc15a0889431b0d08d74c6beb4b49fa0'
